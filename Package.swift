171d75e03d97198c1aa37933b4ffc9bec6b413c9
cal <cal@open.store>
2022-06-21 15:31:02 -0400

    Update to modern spm format

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Swiftline"
+    name: "Swiftline",
+    products: [
+        .library(name: "SwiftLine", targets: ["SwiftLine"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftLine",
+            path: "Source/"
+        )
+    ]
 )
